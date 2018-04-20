2c2bbeeaed367f363b757afd59bdbf536e4b0aec
richardottinger <richard.ottinger@eastexcrude.com>
2018-04-20 08:57:32 -0500

    Working with php Arrays

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
 <?php
 // this file will receive and prepare the data
-$greeting = "Hello, " . htmlspecialchars($_GET['name']);
+//$greeting = "Hello, " . htmlspecialchars($_GET['name']);
 // then the following file will render the data
 // this is a highlevel concept of separatation of concerns
-require 'index.view.php';
+//require 'index.view.php';
+
+
+//load some data
+$names = [
+	'Jeff',
+	'John',
+	'Mary'
+];
+
+// foreach ($names as $name) {
+// 	echo $name . ',';
+// }
+
+// load some view
+require 'index.view.php';  // within the view you cycle through that array and echoed out the name wrapped in a list item 
